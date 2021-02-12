4d4b8b14a4aa612a2ac4786d5850814865a279c6
Luke Scanlan <lukescanlanatwork@gmail.com>
2021-02-12 16:29:54 +0000

    Add comments
    This is to help my own understanding.

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -4,6 +4,7 @@ public class App
 {
     public static void main(String[] args)
     {
+//      Just checking that the main method is run with a console log
         System.out.println("nice wan");
     }
 }
